4af22007bd3c75698f4fd6d920a7bb51a96443b4
Kwangmin Bae <3457709@naver.com>
2019-02-22 14:41:51 +0900

    remove pods

--- Pods/Target Support Files/Carte/Carte-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Carte : NSObject
-@end
-@implementation PodsDummy_Carte
-@end
--- Pods/Target Support Files/RxSkeleton/RxSkeleton-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxSkeleton : NSObject
-@end
-@implementation PodsDummy_RxSkeleton
-@end
